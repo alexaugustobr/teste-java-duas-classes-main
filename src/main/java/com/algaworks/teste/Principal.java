01c52ad8f1f5b0fcac067993c7779e3e5a7a3ed8
Alex Augusto <alexaugustoti@gmail.com>
2024-02-13 10:11:20 -0300

    first commit

---
+++ src/main/java/com/algaworks/teste/Principal.java
@@ -0,0 +1,9 @@
+package com.algaworks.teste;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Classe principal");
+    }
+
+}
---
+++ src/main/java/com/algaworks/teste/filho/Filho.java
@@ -0,0 +1,7 @@
+package com.algaworks.teste.filho;
+
+public class Filho {
+	public static void main(String[] args) {
+		System.out.println("Classe filho");
+	}
+}
